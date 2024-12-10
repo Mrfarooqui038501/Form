6e53ece264876e92d36d2af3b775ba181f0833b7
Arman <armanfarooqui078601@gmail.com>
2024-12-10 12:05:16 +0530

    Update formController.js

--- server/controllers/formController.js
+++ server/controllers/formController.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 exports.createForm = async (req, res) => {
   try {
       const { title } = req.body;
-      const headerImage = req.file ? req.file.path : null; // Get the header image path
-      // Parse questions from the request body
+      const headerImage = req.file ? req.file.path : null; 
+      
       const questions = Object.keys(req.body)
           .filter(key => key.startsWith('questions['))
           .map(key => JSON.parse(req.body[key]));
@@ -16,7 +16,7 @@ exports.createForm = async (req, res) => {
       const savedForm = await newForm.save();
       res.status(201).json(savedForm);
   } catch (error) {
-      console.error('Error creating form:', error); // Log the error for debugging
+      console.error('Error creating form:', error); 
       res.status(400).json({ message: error.message });
   }
 };
@@ -30,7 +30,7 @@ exports.getAllForms = async (req, res) => {
 };
 exports.getFormById = async (req, res) => {
   try {
-    const form = await Form.findById(req.params.id); // Find form by ID
+    const form = await Form.findById(req.params.id); 
     if (!form) {
       return res.status(404).json({ message: 'Form not found' });
     }
